109682645684b9014a3d1bb4e61d30920dd74c40
Paulo Lima <prmlimajr@hotmail.com>
2020-08-20 20:25:58 -0300

    Fix no post sleep

--- src/app/controllers/SleepController.js
+++ src/app/controllers/SleepController.js
@@ -14,15 +14,29 @@ class SleepController {
 
     const { hours, day } = req.body;
 
+    const sleepExists = await connection('sleep')
+      .select('sleep.*')
+      .where('day', day);
+
+    const sleptToday =
+      sleepExists.length > 0 ? Number(sleepExists[0].hours) + hours : hours;
+
     const sleep = {
       user_id: req.userId,
-      hours,
+      hours: sleptToday,
       day,
     };
 
-    const insertedSleep = await connection('sleep').insert(sleep);
+    const insertedSleep =
+      sleepExists.length > 0
+        ? await connection('sleep').update(sleep).where('day', day)
+        : await connection('sleep').insert(sleep);
 
     return res.json({ ...sleep });
+  }
+
+  async update(req, res) {
+    const { hours, day } = req.body;
   }
 }
 
